30b0d98f028650d1048510f5219f6946cd2def60
AMARNATH K <46416914+Amarnath9962@users.noreply.github.com>
2021-03-30 18:13:23 +0530

    Add files via upload
    Here i given the palindrome ,star(forward and reverse ),leap year, Factorial, Number adding into list ,programs are coded in this repository. Please check it out...Thank you

---
+++ Basic Programs.py
@@ -0,0 +1,54 @@
+# text = input("Please enter the Value or Number :")
+# New = text[::-1]
+# print(New)
+# if New==text:
+#     print("The text is Palindrome ",text)
+# else:
+#     print("The text is not palindrome",text)
+
+# # Printing the * Function in
+# Var = int(input("Please enter the text :"))
+# for x in range(Var+1):
+#     print("* "*x)
+# for y in range(Var,0,-1):
+#     print("* "*y)
+
+# # How to print the factorial of the given number :
+#
+# Num = int(input("Please enter the number:"))
+# data = 1
+# for x in range(1,Num+1):
+#     data = data*x
+# print(data)
+
+# var = int(input("Please enter the number :"))
+# data = 1
+# Out = []
+# for x in range(1,var+1):
+#     data = data * x
+#     Out.append(data)
+# # [1,2,3,4,5]
+# print(Out)
+# New = []
+# for j in range(1,var+1):
+#     data_1 = j*(j+1)
+#     New.append(data_1)
+#
+# print(New)
+#
+
+
+# #  How to check given year is leap year or not .
+#
+# year = int(input("Please enter the Year :"))
+# import calendar
+# if year%4==0 and(year%100!=0 or year%400==0):
+#     print("Its a leap year ",year)
+# else:
+#     print("Its not leap year",year)
+#
+# print(calendar.month(year,2))
+
+
+
+
